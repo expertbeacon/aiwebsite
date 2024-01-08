b17276248680ef09dae21d10bf0a8ab7c5537693
expertbeacon <yonglivelo@gmail.com>
2024-01-08 23:03:19 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,19 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script id="inline-script" dangerouslySetInnerHTML={{ __html: `window.EXCALIDRAW_ASSET_PATH = "/"` }} />
+    <title>HTML Generator</title>
+    <meta name="description" content="Free AI-Powered Website Code Creation"/>
+    <meta name="robots" content="index, follow"/>
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="HTML Generator - Free AI-Powered Website Code Creation" />
+    <meta property="og:description" content="Streamline web design with HTMLGenerator.ai: Convert ideas and images to HTML code instantly using AI, with Tailwind, React, and Vue support." />
+    <meta property="og:url" content="https://www.htmlgenerator.ai/" />
+    <meta property="og:site_name" content="HTML Generator" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="HTML Generator - Free AI-Powered Website Code Creation" />
+    <meta name="twitter:description" content="Streamline web design with HTMLGenerator.ai: Convert ideas and images to HTML code instantly using AI, with Tailwind, React, and Vue support." />
+    <script id="inline-script" dangerouslySetInnerHTML={{ __html: `window.EXCALIDRAW_ASSET_PATH = "/"` }} />
       </Head>
       <body>
         <Main />
