5f8fca13981a17affceb3f0e9dfdc258165f1a30
expertbeacon <yonglivelo@gmail.com>
2024-01-10 10:59:41 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,7 @@ export default function Document() {
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:title" content="HTML Generator - Free AI-Powered Website Code Creation" />
     <meta name="twitter:description" content="Streamline web design with HTMLGenerator.ai: Convert ideas and images to HTML code instantly using AI, with Tailwind, React, and Vue support." />
-    <script id="inline-script" dangerouslySetInnerHTML={{ __html: `window.EXCALIDRAW_ASSET_PATH = "/"` }} />
+        <script id="inline-script" dangerouslySetInnerHTML={{ __html: `window.EXCALIDRAW_ASSET_PATH = "/"` }} />
       </Head>
       <body>
         <Main />
